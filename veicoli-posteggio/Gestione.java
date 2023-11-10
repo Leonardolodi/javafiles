8f8f3224bffc9f438144e7b0ff2e192e59d17da0
Leonardolodi <leonardo03.lodi@edu.unife.it>
2023-11-10 11:30:06 +0100

    mod

--- veicoli-posteggio/Gestione.java
+++ veicoli-posteggio/Gestione.java
@@ -7,7 +7,7 @@ public class Gestione {
         static List<Cliente> clienti   = new LinkedList<Cliente>();
         static Map<Integer,String> codTarga = new HashMap<Integer,String>();
         static Map<Integer,Double> codCosto = new HashMap<Integer,Double>();
-        static Map<String,Integer> targaNoleggi = new HashMap<String,Integer>();
+        static Map<Integer,Double> codTot = new HashMap<Integer,Double>();
 
     public static void main(String[] args) {
 
@@ -21,32 +21,33 @@ public class Gestione {
                 int cod=Integer.parseInt(tok.nextToken());
                 String tipo=tok.nextToken();
                 String targa=tok.nextToken();
-                line                = br.readLine();
-                String modello = line;
-                line                = br.readLine();
-                String marca = line;
                 line= br.readLine();
                     if (tipo.equals("auto")) {
                         tok = new StringTokenizer(line);
-                        double bag=Double.parseDouble(tok.nextToken());
                         int cil=Integer.parseInt(tok.nextToken());
-                        line= br.readLine();
-                        String categoria= line;
-                        double costo=Double.parseDouble(br.readLine());
-                        Auto t= new Auto(cod, targa, modello, marca, costo, cil, bag, categoria);
+                        boolean diesel=Boolean.parseBoolean(tok.nextToken());
+                        line                = br.readLine();
+                        String modello = line;
+                        line                = br.readLine();
+                        String marca = line;
+                        Auto t=new Auto(cod,targa,modello,marca,cil,diesel);
                         veicoli.add(t);
                         codTarga.put(cod,targa);
-                        codCosto.put(cod,costo);
+                        codTot.put(cod,0.0);
+                        codCosto.put(cod,10.0);
                     } else {
-                        tok = new StringTokenizer(line);
-                        boolean rimorchio=Boolean.parseBoolean(tok.nextToken());
-                        int posti=Integer.parseInt(tok.nextToken());
-                        boolean carico=Boolean.parseBoolean(tok.nextToken());
-                        double costo=Double.parseDouble(br.readLine());
-                        Furgone t=new Furgone(cod,targa,modello,marca,costo,posti,rimorchio,carico);
+                        String cat=line;
+                        line=br.readLine();
+                        int posti=Integer.parseInt(line);
+                        line                = br.readLine();
+                        String modello = line;
+                        line                = br.readLine();
+                        String marca = line;
+                        Furgone t=new Furgone(cod,targa,modello,marca,posti,cat);
                         veicoli.add(t);
                         codTarga.put(cod,targa);
-                        codCosto.put(cod,costo);
+                        codTot.put(cod,0.0);
+                        codCosto.put(cod,15.0);
                     }
                     line = br.readLine();
                 }
@@ -60,27 +61,23 @@ public class Gestione {
         //PUNTO 2: lettura di tutti i ricercatori
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader("clienti.txt"));
+            BufferedReader br = new BufferedReader(new FileReader("posteggi.txt"));
             String line=br.readLine();
             while (line!= null){
                 String nomeCog=line;
                 Cliente c=new Cliente(nomeCog);
                 clienti.add(c);
                 line=br.readLine();
-                while (line!=null && !line.equals("")){
-                    StringTokenizer tok = new StringTokenizer(line);
+                StringTokenizer tok = new StringTokenizer(line);
+                while (tok.hasMoreElements()){
                     int cod=Integer.parseInt(tok.nextToken());
                     int giorni=Integer.parseInt(tok.nextToken());
                     String targa=codTarga.get(cod);
                     double costo=codCosto.get(cod);
-                    c.addNoleggio(targa,giorni,costo);
-                    Integer noleggi=targaNoleggi.get(targa);
-                    if (noleggi==null)
-                        targaNoleggi.put(targa,1);
-                    else
-                        targaNoleggi.put(targa,noleggi+1);
-                    line=br.readLine();
-            }
+                    c.addPosteggio(targa,giorni,costo);
+                    double current=codTot.get(cod);
+                    codTot.put(cod,current+giorni*costo);
+                }
                 line=br.readLine();
             }
            br.close();
@@ -91,27 +88,17 @@ public class Gestione {
 
 
         //PUNTO 3: stampa di tutti i veicoli
-        System.out.println("tipo, targa, codice, modello, marca, costo giornaliero, bagagliaio, cilindrata, categoria, rimorchio, numero di posti, vano di carico");
+        System.out.println("tipo, targa, codice, modello, marca, cilindrata, diesel, categoria, numero di posti");
         for (Veicolo v : veicoli)
             System.out.println(v);
         System.out.println();
         //PUNTO 4: stampa clienti
         for (Cliente c : clienti)
           System.out.println(c);
-
         //PUNTO 5: stampe del totale dei noleggi
         System.out.println();
-        try {
-            BufferedReader kbd =
-            new BufferedReader(new InputStreamReader(System.in));
-            String targa=kbd.readLine();
-            
-
-        int n=targaNoleggi.get(targa);
-                System.out.println(n);
-            }
-            catch (IOException e) {
-               System.err.println(e);
-           }
+        int targa = Integer.parseInt(args[0]);
+        double tot=codTot.get(targa);
+                System.out.println(targa+" "+tot);
     }
 }
